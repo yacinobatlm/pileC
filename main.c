b011bf23ef615246781b53b4813e09a04f242034
Yacino <yacinobatlm13@gmail.com>
2020-07-11 00:03:10 +0100

    First Update

--- main.c
+++ main.c
@@ -4,13 +4,19 @@ int main()
 {
     Pile p;
     p = newPile();
-    printf("\n%d\n",topPile(p));
+    int n;
+    int* t = pushData(&n);
+    p = pushDataPile(p, t, n);
+    printf("--------------------");
     printPile(p);
+    /*printPile(p);
     p = pushPile(p,10);
     p = pushPile(p,20);
     p = pushPile(p,0);
     p = pushPile(p,40);
     printPile(p);
-    printf("\n%d\n",topPile(p));
+    printf("-------------\n");
+    p = clearPile(p);
+    printPile(p);*/
     return 0;
 }
--- pile.h
+++ pile.h
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+int numberOfElement;
+
 /*---------------------------Declaration d'une pile--------------------------------*/
 
 typedef struct pile
@@ -34,16 +37,33 @@ Pile pushPile(Pile p, int x)
     return newPile;
 }
 
-/*---------------------------Retourner le sommet de la pile--------------------------------*/
+/*---------------------------Crée un tableau dinamique pour remplire les données--------------------------------*/
 
-int topPile(Pile p)
+int* pushData(int* numberOfElement)
 {
-    if (isEmptyPile(p))
-        return -1; //il retourne rien mais on suppose que nos données est des nombres positif 
-    return p->donnee;
+    printf("Entrer la taille de tableau: ");
+    scanf("%d",numberOfElement);
+    int* t = (int*)malloc(*numberOfElement * sizeof(int));
+    for (int i = 0; i < *numberOfElement; i++)
+    {
+        printf("T[%d] = ",i);
+        scanf("%d",t + i);
+    }
+    return t;
 }
 
-/*---------------------------Crée une variable de type pile--------------------------------*/
+/*---------------------------pile--------------------------------*/
+
+Pile pushDataPile(Pile p ,int* t, int numberOfElement)
+{
+    for (int i = 0; i < numberOfElement; i++)
+    {
+        p = pushPile(p, *(t + i));
+    }
+    return p;
+}
+
+/*---------------------------Afficher la pile--------------------------------*/
 
 
 void printPile(Pile p)
@@ -60,8 +80,33 @@ void printPile(Pile p)
     }
 }
 
-/*---------------------------------------------------------------------*/
+/*---------------------------Retourner le sommet de la pile--------------------------------*/
 
-/*---------------------------------------------------------------------*/
+int topPile(Pile p)
+{
+    if (isEmptyPile(p))
+        return -1; //il retourne rien mais on suppose que nos données est des nombres positif 
+    return p->donnee;
+}
+
+/*---------------------------Vider le sommet de la Pile--------------------------------*/
+Pile popPile(Pile p)
+{
+    Pile tmp = newPile();
+    tmp = p->next;
+    free(p);
+    p = tmp;
+    return p;
+}
+
+/*---------------------------Vider la Pile--------------------------------*/
+
+Pile clearPile(Pile p)
+{
+    if(isEmptyPile(p))
+        return newPile();
+    p = popPile(p);
+    return clearPile(p);
+}
 
 /*---------------------------------------------------------------------*/
